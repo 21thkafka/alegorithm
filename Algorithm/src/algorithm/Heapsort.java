2bf16f5b993d4d2d9a87d0cc3bcfdd6040b2acbc
caudeutsch@gmail.com <82437848+21thkafka@users.noreply.github.com>
2021-11-24 23:35:30 +0900

    heap 개념

---
+++ Algorithm/src/algorithm/Heapsort.java
@@ -0,0 +1,9 @@
+package algorithm;
+
+public class Heapsort {
+	//최악의 경우 시간복잡도 O(nlog2n), 추가 배열 불필요, 이진 힙 자료구조를 사용
+	//complete binary tree(full이 아님)이면서 heap property를 만족해야
+	//max heap property: 부모는 자식보다 크거나 값다
+	//or min heap property: 부모는 자식보다 작거나 같다
+	
+}
