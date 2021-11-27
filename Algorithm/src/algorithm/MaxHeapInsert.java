f66b7c55ffc7f61a6157d58cb2506c79fd82b8b8
caudeutsch@gmail.com <82437848+21thkafka@users.noreply.github.com>
2021-11-27 23:53:29 +0900

    heap정렬 변형 insert

---
+++ Algorithm/src/algorithm/MaxHeapInsert.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+public class MaxHeapInsert {
+
+	/*
+	 * MAX-HEAP-INSERT(A, key){
+	 * 	heap_size = heap_size+1;
+	 * 	A[heap_size] = key;
+	 * 	i = heap_size;
+	 * 	while ( i>1 and A[PARENT(i) < A[i]){
+	 * 		exchange A[i] and A[PARENT(i)];
+	 * 		i = PARENT(i);
+	 * 	}
+	 * }
+	 */
+}
