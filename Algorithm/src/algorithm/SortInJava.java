c82be31914ee1a4514af4158667186bf0f992122
caudeutsch@gmail.com <82437848+21thkafka@users.noreply.github.com>
2021-12-03 23:39:10 +0900

    Comparator 복수의 기준 정렬 인터페이스

--- Algorithm/src/algorithm/SortInJava.java
+++ Algorithm/src/algorithm/SortInJava.java
@@ -29,6 +29,7 @@ public class Fruit implements Comparable<Fruit>{
 	 }	
 	 */
 	}
+
 }
 	Fruit [] fruits = new Fruit[4];
 	fruits [0] = new Fruit("Pineapple", 70);
@@ -37,4 +38,45 @@ public class Fruit implements Comparable<Fruit>{
 	fruits [3] = new Fruit("Banana", 90);
 	
 	Arrays.sort(fruits);
+	}
+	
+	// 하나의 객체 타입에 대해서 2가지 이상의 기준으로 정렬을 지원하려면 Comparator를 사용
+	Comparator<Fruit> nameComparator = new Comparator<Fruit>() {
+		public int compare(Fruit fruit1, Fruit fruit2) {
+			return fruit1.name.compareTo(fruit2.name);
+		}
+	};
+	Comparator<Fruit> quantComparator = new Comparator<Fruit>() {
+		public int compare(Fruit fruit1, Fruit fruit2) {
+			return fruit1.quantity - fruit2.quantity;
+		}
+	};
+	
+	//Comparator interface를 implement하는 어떤 class의 객체
+	Arrays.sort(fruits, nameComparator);
+	//or
+	Arrays.sort(fruits, quantComparator);	
+	
+	//Comparator를 사용할때 가장 깔끔한 형태. 
+	/*
+	 * public class Fruit{
+	 * 	public String name;
+	 * 	public int quantity;
+	 * 	public Fruit(String name, int quantity){
+	 * 		this.name = name;
+	 * 		this.quantity = quantity;
+	 * }
+	 * 사실상 규칙이므로 클래스 공유하며 쓰기 때문에 static을 붙여줌
+	 * public static Comparator<Fruit> nameComparator = new Comparator<Fruit>(){
+	 * 		public int compare(Fruit fruit1, Fruit fruit2){
+	 * 			return fruit1.name.compareTo(fruit2.name);
+	 * 		}
+	 * }
+	 * public static Comparator<Fruit> quantComparator = new Comparator<Fruit>(){
+	 * 		public int compare(Fruit fruit1, Fruit fruit2){
+	 * 			return fruit1.quantity - fruit2.quantity;
+	 * 		}
+	 * };
+	 * }
+	 */
 }
